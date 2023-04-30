e6fb9f15655146a1a86684c59d6567e30eb44cf8
oNovlaSliveDNo <novosel.dan@gmail.com>
2023-05-01 05:23:47 +1000

    Update Drawing all.py

--- Drawing all.py
+++ Drawing all.py
@@ -1,80 +1,111 @@
 import os
-os.system('CLS')
 
-COLS = 155
-ROWS = 45
 
-class admin_plot():
+class headline():
+    def __init__(self):
+        pass
 
-    def __init__(self, COLS:int, ROWS:int):
+    def create_headline(self, COLS:int, HEAD:str):
+        self.COLS = COLS
+        self.HEAD = HEAD
+        SPACE_before = int( (COLS + 1 - len(HEAD)) / 2)
+        SPACE_after = int(COLS + 1 - SPACE_before - len(HEAD))
+        return (SPACE_before*'\033[44m\033[37m\033[1m ') + (HEAD) + (SPACE_after*' ') + ('\n')
+
+
+class authors():
+    def __init__(self):
+        pass
+
+    def create_authors(self, SPACE:int, COLS:int, admins:tuple):
+        self.SPACE = SPACE
+        self.COLS = COLS
+        self.admins = admins
+        s = ''
+        for name in admins:
+            s += (name + SPACE*' ')
+        space_before = int( (COLS + 1 - len(s)) / 2)
+        space_after = int(COLS + 1 - space_before - len(s))
+        return ('\033[44m\033[37m\033[1m '*space_before) + (s) + (' '*space_after) + ('\033[0m')
+
+
+class tab():
+    def __init__(self):
+        pass
+
+    def create_tab(self, COLS:int, SPACE:int, TAB:tuple):
+        self.COLS = COLS
+        self.SPACE = SPACE
+        self.TAB = TAB
+        st = str()
+        for tab in TAB:
+            st += (' ' * SPACE) + tab
+        return (st) + (' '*int(COLS + 1 - len(st))) + ('\033[0m\n')
+
+
+class table():
+    def __init__(self):
+        pass
+
+    def create_table(self, COLS: int, ROWS: int):
         self.COLS = COLS
         self.ROWS = ROWS
 
-        self.plot = str()
-        self.HEAD = 'ADMINISTRATOR SETUP MODE'
-        self.SPACE = 4
-        self.TABS = ('Main', 'Edit', 'Movie_Poster', 'Exit')
+        plot = ''
 
+        for row in range(ROWS):
+            for col in range(COLS+1):
 
-        for row in range(ROWS + 1):
-            for col in range(COLS + 1):
-                if (row == 0):
-                    SPACE_before = int((COLS + 1 - len(self.HEAD)) / 2)
-                    SPACE_after = int(COLS + 1 - SPACE_before - len(self.HEAD))
-                    self.plot += (SPACE_before * '\033[44m\033[37m\033[1m ') + (self.HEAD) + (SPACE_after * ' ') + ('\n')
-                    break
-                if (row == 1):
-                    st = str()
-                    for tab in self.TABS:
-                        st += (' ' * self.SPACE) + tab
-                    self.plot += st + ' ' * int(COLS+1 - len(st)) + ('\033[0m')
-                    break
-
-                if (row == 3) and (col == 0):
-                    self.plot += '\033[47m\033[34m'
-                    self.plot += '┏'
-                if (row == 3) and (col == COLS):
-                    self.plot += '┓'
+                if (row == 0) and (col == 0):
+                    plot += '\033[47m\033[34m'
+                    plot += '┏'
+                if (row == 0) and (col == COLS):
+                    plot += '┓'
                 if (row == ROWS-1) and (col == 0):
-                    self.plot += '┗'
+                    plot += '┗'
                 if (row == ROWS-1) and (col == COLS):
-                    self.plot += '┛\033[44m\033[37m'
+                    plot += '┛\033[44m\033[37m'
 
-                if (row == 3 or row == ROWS-1) and (0 < col < COLS):
-                    self.plot += '━'
-                if (3 < row < ROWS-1) and (col == 0 or col == COLS):
-                    self.plot += '┃'
 
-                if (3 < row < ROWS-1) and (0 < col < COLS):
-                    self.plot += ' '
-    
+                if (row == 0 or row == ROWS-1) and (0 < col < COLS):
+                    plot += '━'
+                if (0 < row < ROWS-1) and (col == 0 or col == COLS):
+                    plot += '┃'
+
+
+                if (0 < row < ROWS-1) and (0 < col < COLS):
+                    plot += ' '
+
+
                 if col == COLS:
-                    self.plot += '\n'
+                    plot += '\n'
+        return plot
 
 
-                if row == ROWS:
-                    admins = (
-                    'Читаева Анастасия', 'Чучунова Юлия', 'Добрева Валентина', 'Тишкин Алексей', 'Праведников Илья',
-                    'Новоселов Данил')
-                    s = ''
-                    for name in admins:
-                        s += (name + self.SPACE*' ')
-                    space_before = int((COLS+1-len(s))/2)
-                    space_after = int(COLS + 1 - space_before - len(s))
-                    self.plot += ('\033[44m\033[37m\033[1m '*space_before) + (s) + (' '*space_after) + ('\033[0m')
-                    break
-
-
-    def draw(self):
-        print(self.plot)
-
-
-
-object = admin_plot(COLS, ROWS)
 while True:
-    os.system('CLS')
-    object.draw()
+    os.system("CLS")
+
+    COLS = 155
+    ROWS = 43
+    SPACE = 4
+    HEADSTROKE = 'ADMINISTRATOR SETUP MODE'
+    TABS = ('Main', 'Edit', 'Movie_Poster', 'Exit')
+    AUTHORS = ('Читаева Анастасия', 'Чучунова Юлия', 'Добрева Валентина', \
+                'Тишкин Алексей', 'Праведников Илья', 'Новоселов Данил')
+
+    Headline = headline()
+    Tab = tab()
+    Table = table()
+    Authors = authors()
+
+    objects = (Headline.create_headline(COLS, HEADSTROKE), \
+               Tab.create_tab(COLS, SPACE, TABS), \
+               Table.create_table(COLS, ROWS), \
+               Authors.create_authors(SPACE, COLS, AUTHORS))
+
+    plot = str()
+    for object in objects:
+        plot += object
+
+    print(plot, end='')
     input()
-
-
-
